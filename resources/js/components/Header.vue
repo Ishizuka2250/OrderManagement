b0bfb2c4d313f68ed5e2a7658e3e75afc9b90fad
Ishizuka2250 <squid1028@gmail.com>
2022-02-22 15:50:21 +0900

    [Feature]Login画面のUIを実装 [Add]Headerコンポーネントを追加 (Header.vue) [Edit]app.cssを変更 (既存cssをごっそり削除, body:background #CCC を追記

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <h1 class="header-text">CUT SHOP FREESTYLE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .header {
+    width: 100%;
+    background: black;
+    display: flex;
+    justify-content: center;
+  }
+  .header-text{
+    display: inline-block;
+    color: gold;
+    font-size: 1.8em;
+    margin: 0;
+    padding: 20px 0px;
+  }
+</style>
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,16 +1,82 @@
 <template>
   <div class="container">
-    <h1>login</h1>
+    <AppHeader></AppHeader>
+    <div class="outer-login-container">
+      <div class="login-container">
+        <p class="login-header-text">管理者画面ログイン</p>
+        <table>
+          <tr>
+            <th>UserName</th>
+            <td><input class="input-box" type="text"></td>
+          </tr>
+          <tr>
+            <th>Password</th>
+            <td><input class="input-box" type="password" name="" id=""></td>
+          </tr>
+        </table>
+        <div class="login-button-row">
+          <button class="login-button">Login</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import header from '../components/Header.vue'
 export default {
+  components: {
+    AppHeader: header
+  },
   data() {
   }
 }
 </script>
 
 <style>
-
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .outer-login-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 100px;
+  }
+  .login-container{
+    display: inline-block;
+    padding: 20px;
+    background: white;
+    border-radius: 10px;
+  }
+  .login-header-text {
+    margin-bottom: 10px;
+    text-align: center;
+    font-size: 1.3em;
+    font-weight: bold;
+  }
+  tr th {
+    padding: 10px;
+    font-size: 1.1em;
+  }
+  tr td {
+    padding: 10px;
+  }
+  .input-box {
+    padding: 10px;
+    font-size: 1.1em;
+  }
+  .login-button-row {
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  .login-button {
+    display: inline-block;
+    margin-right: 10px;
+    padding: 5px 15px;
+    font-size: 1.1em;
+  }
 </style>
