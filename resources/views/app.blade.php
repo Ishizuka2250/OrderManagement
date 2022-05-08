deb49c1156d9c7831529ec728d8063b410993cb5
Ishizuka2250 <squid1028@gmail.com>
2022-05-08 13:55:07 +0900

    [Debug] app.blade.php - mixedでcssが適用されない件を修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('css/reset.css')}}">
-  <link rel="stylesheet" href="{{ asset('css/body.css')}}">
-  <link rel="stylesheet" href="{{ asset('css/awesome_notifications.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('css/reset.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('css/body.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('css/awesome_notifications.css')}}">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
