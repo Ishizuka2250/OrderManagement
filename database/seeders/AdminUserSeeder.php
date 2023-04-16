6bec969ee062d345b12832131e480fc8017737ab
Ishizuka2250 <squid1028@gmail.com>
2023-04-16 23:16:14 +0900

    [Modify] User.php AdminUserSeeder.php [Create] 2023_04_16_162054_add_column_to_users_table_master_key.php - ユーザーテーブルmaster_keyカラム追加対応

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use illuminate\Support\Str;
 
 class AdminUserSeeder extends Seeder
 {
@@ -21,6 +22,13 @@ class AdminUserSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make('adminuser'),
+            'master_key' =>  strtoupper(Str::random(32))
+        ]);
+        User::where('name', 'felica')->delete();
+        User::create([
+            'name' => 'felica',
+            'email' => 'felica@felica.com',
+            'password' => Hash::make('felicauser'),
         ]);
     }
 }
